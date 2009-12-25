19cf855ef0171533569a8b765003634a9702a2c4
Abhishek shukla <abhishekpshukla@gmail.com>
2009-12-25 23:59:06 +0530

    updated a dashboard

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,31 +1,13 @@
 class Admin::UsersController < ApplicationController
   before_filter :require_user
   before_filter :is_admin?
-
-  def index
-    @employees = User.find_all_user_except_admin
-    respond_to do |format|
-      format.html {}
-      format.js {}
-      format.xml { render :xml => @employees }
-    end
-  end
+  resource_controller
   
-  def new
-    @user = User.new
+  create.wants.html { redirect_to admin_employees_path }
+
+  index.before do
+    @employees = User.find_all_user_except_admin
   end
 
-  def create
-    @user = User.new(params[:user])
-    respond_to do |format|
-      if @user.save_without_session_maintenance
-         flash[:notice] = t("admin.user.employee_add")
-        format.html { redirect_to admin_employees_path }
-        format.xml { render :xml => @user }
-      else
-        format.html { render :new }
-      end
-    end
-  end
-
+  
 end
