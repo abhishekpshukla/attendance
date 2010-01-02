31b1d7212a8784efe67ae891c8a5bf696504f9ab
Abhishek shukla <abhishekpshukla@gmail.com>
2010-01-02 11:01:54 +0530

    fixed the out time error

--- app/controllers/employee/time_sheets_controller.rb
+++ app/controllers/employee/time_sheets_controller.rb
@@ -8,6 +8,10 @@ class Employee::TimeSheetsController < ApplicationController
     @todays_time_sheet          = current_user.user_detail.time_sheets.find(:first, :conditions => ["DATE(in_time) = ?", Time.now.to_date])
     @time_sheet                 = @todays_time_sheet.blank? ? TimeSheet.new : @todays_time_sheet
   end
+
+  index.before do
+    @time_sheets = current_user.user_detail.time_sheets.paginate(:page => params[:page], :per_page => 10)
+  end
   
   create.before do
     @time_sheet.user_detail_id  = current_user.user_detail.id
@@ -18,5 +22,11 @@ class Employee::TimeSheetsController < ApplicationController
   create.flash "Timesheet added"
   
   create.wants.html { redirect_to employee_time_sheets_path }
-  
+
+  update do
+    failure do
+      wants.html { render :action => :new }
+    end
+  end
+
 end
--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -1,5 +1,13 @@
 class TimeSheet < ActiveRecord::Base
   belongs_to :user_detail, :foreign_key => :user_detail_id
 
-  
+  private
+  def validate
+   if !self.in_time.nil? && !self.out_time.nil?
+     if self.in_time > self.out_time
+       self.errors.add_to_base("Please select the proper time.")
+     end
+   end
+  end
+
 end
